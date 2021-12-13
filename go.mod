136c4ce15209736361363057a63648b0c15f835d
dzw <hu.duanzhiwen@aisino.com>
2021-12-13 17:51:41 +0800

    update

--- go.mod
+++ go.mod
@@ -17,9 +17,11 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
@@ -37,4 +39,5 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,57 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/awensir/go-aurora/aurora"
 	"log"
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
+	"time"
 )
-
-type Mm1 struct {
-}
-
-func (m *Mm1) PreHandle(c *aurora.Ctx) bool {
-	log.Println("PreHandle Mm1")
-	return true
-}
-
-func (m *Mm1) PostHandle(c *aurora.Ctx) {
-	log.Println("PostHandle Mm1")
-}
-
-func (m *Mm1) AfterCompletion(c *aurora.Ctx) {
-	log.Println("AfterCompletion Mm1")
-}
-
-type Mm2 struct {
-}
-
-func (m *Mm2) PreHandle(c *aurora.Ctx) bool {
-	log.Println("PreHandle Mm2")
-	return true
-}
-
-func (m *Mm2) PostHandle(c *aurora.Ctx) {
-	log.Println("PostHandle Mm2")
-}
-
-func (m *Mm2) AfterCompletion(c *aurora.Ctx) {
-	log.Println("AfterCompletion Mm2")
-}
-
-type Mm3 struct {
-}
-
-func (m *Mm3) PreHandle(c *aurora.Ctx) bool {
-	log.Println("PreHandle Mm3")
-	return true
-}
-
-func (m *Mm3) PostHandle(c *aurora.Ctx) {
-	log.Println("PostHandle Mm3")
-}
-
-func (m *Mm3) AfterCompletion(c *aurora.Ctx) {
-	log.Println("AfterCompletion Mm3")
-}
 
 func main() {
 
@@ -60,11 +16,25 @@ func main() {
 
 	// GET 方法注册 web get请求
 	a.GET("/", func(c *aurora.Ctx) interface{} {
-		log.Println("service..")
+		accept, err := websocket.Accept(c.Response, c.Request, nil)
+		if err != nil {
+			return err
+		}
+		ctx, cancel := context.WithTimeout(c.Request.Context(), time.Second*10)
+		defer cancel()
+		var v interface{}
+		err = wsjson.Read(ctx, accept, &v)
+		if err != nil {
+			return err
+		}
+		log.Printf("接收到客户端：%v\n", v)
+		err = wsjson.Write(ctx, accept, "Hello WebSocket Client")
+		if err != nil {
+			return err
+		}
+		accept.Close(websocket.StatusNormalClosure, "")
 		return nil
 	})
-
-	a.RouteIntercept("/", &Mm1{}, &Mm2{}, &Mm3{})
 
 	// 启动服务器 默认端口8080，更改端口号 a.Guide(”8081“) 即可
 	a.Guide()
